5636012f0aa377fdd21ec8d70948de8d6ec981ac
Arun Singh <arunsinghsnd@gmail.com>
2021-06-01 00:09:28 +0530

    user creation done succesfully

--- src/models/user.js
+++ src/models/user.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       emun: ["user", "admin"],
-      default: "admin",
+      default: "user",
     },
     contactNumber: {
       type: String,
