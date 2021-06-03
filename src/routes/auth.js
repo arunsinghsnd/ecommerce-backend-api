f009c4ff9b178cf3c5f4d16e725a2f6ab16e618b
Arun Singh <arunsinghsnd@gmail.com>
2021-06-03 21:27:57 +0530

    validator checked with signin function

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const { signup, signin, requireSignin } = require("../controllers/auth");
-const { validateRequest, isRequestValidated } = require("../validators/auth");
+const {
+  validateSignupRequest,
+  isRequestValidated,
+  validateSigninRequest,
+} = require("../validators/auth");
 const router = express.Router();
 
-router.post("/signup", validateRequest, isRequestValidated, signup);
-router.post("/signin", signin);
+router.post("/signup", validateSignupRequest, isRequestValidated, signup);
+router.post("/signin", validateSigninRequest, isRequestValidated, signin);
 
 // router.post("/profile", requireSignin, (req, res) => {
 //   res.status(200).json({ user: "Profile" });
--- src/validators/auth.js
+++ src/validators/auth.js
@@ -1,8 +1,15 @@
 const { check, validationResult } = require("express-validator");
 
-exports.validateRequest = [
+exports.validateSignupRequest = [
   check("firstName").notEmpty().withMessage("firstName is required"),
   check("lastName").notEmpty().withMessage("lastName is required"),
+  check("email").isEmail().withMessage("Valid email is required"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("password must be at least 6 characters long"),
+];
+
+exports.validateSigninRequest = [
   check("email").isEmail().withMessage("Valid email is required"),
   check("password")
     .isLength({ min: 6 })
