e4d786303b50f72828cf5fd5d5565dcf692f7e0c
Arun Singh <arunsinghsnd@gmail.com>
2021-06-04 13:52:02 +0530

    changed the categories controller file

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,25 +1,8 @@
 const express = require("express");
-const Category = require("../models/category");
-const slugify = require("slugify");
+
+const { addCategory } = require("../controllers/category");
 const router = express.Router();
 
-router.post("/category/create", (req, res) => {
-  const categoryObj = {
-    name: req.body.name,
-    slug: slugify(req.body.name),
-  };
-
-  if (req.body.parentId) {
-    categoryObj.parentId = req.body.parentId;
-  }
-
-  const cat = new Category(categoryObj);
-  cat.save((error, category) => {
-    if (error) return res.status(400).json({ error: error });
-    if (category) {
-      return res.status(200).json({ category });
-    }
-  });
-});
+router.post("/category/create", addCategory);
 
 module.exports = router;
