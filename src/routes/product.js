2f383bf27e0d40769979409b3ba2ab21f3a911c5
Arun Singh <arunsinghsnd@gmail.com>
2021-06-05 20:31:06 +0530

    added the functionality to upload multiple files

--- src/routes/product.js
+++ src/routes/product.js
@@ -8,16 +8,27 @@ const Product = require("../models/product");
 
 const router = express.Router();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+
+const shortid = require("shortid");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, shortid.generate() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 router.post(
   "/product/create",
   requireSignin,
   adminMiddleware,
-  upload.single("productPicture"),
+  upload.array("productPicture"),
   createProduct
 );
-
-//router.get("/category/getcategory", getCategory);
 
 module.exports = router;
