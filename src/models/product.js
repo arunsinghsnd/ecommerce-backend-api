26b7d01c6ea0e883b5eb1d00685d506bb471fb06
Arun Singh <arunsinghsnd@gmail.com>
2021-06-05 21:08:35 +0530

    product creation api compeleted

--- src/models/product.js
+++ src/models/product.js
@@ -15,6 +15,10 @@ const productSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    quantity: {
+      type: Number,
+      required: true,
+    },
     description: {
       type: String,
       required: true,
@@ -30,8 +34,16 @@ const productSchema = new mongoose.Schema(
         review: String,
       },
     ],
-    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     updatedAt: Date,
   },
   { timestamps: true }
