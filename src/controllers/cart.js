9b9a960280280e599be09eb4f2cd523d9919c5d5
Arun Singh <arunsinghsnd@gmail.com>
2021-06-09 23:09:56 +0530

    created the cart api

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,14 +1,34 @@
 const Cart = require("../models/cart");
 
 exports.addItemToCart = (req, res) => {
-  const cart = new Cart({
-    user: req.user._id,
-    cartItems: req.body.cartItems,
-  });
-
-  cart.save((error, cart) => {
+  Cart.findOne({ userId: req.user.id }).exec((error, cart) => {
     if (error) return res.status(400).json({ error });
+    if (cart) {
+      // if cart already exists then update cart by quantity
+      Cart.findOneAndUpdate(
+        { user: req.user._id },
+        {
+          $push: {
+            cartItems: req.body.cartItems,
+          },
+        }
+      ).exec((error, _cart) => {
+        if (error) return res.status(400).json({ error });
+        if (_cart) return res.status(201).json({ _cart });
+      });
+      //res.status(200).json({ message: cart });
+    } else {
+      // if cart does not exists then create a new cart
+      const cart = new Cart({
+        user: req.user._id,
+        cartItems: [req.body.cartItems],
+      });
 
-    if (cart) return res.status(201).json({ cart });
+      cart.save((error, cart) => {
+        if (error) return res.status(400).json({ error });
+
+        if (cart) return res.status(201).json({ cart });
+      });
+    }
   });
 };
